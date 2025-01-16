8f4ddf3a1f4dcba642a882d95588d72a4efbb4ab
Byungjoo Park <byungjoopark@Byungjoos-MacBook-Pro-2.local>
2025-01-16 16:10:50 +0900

    fixing firebase error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import { analytics } from '@/lib/firebase';
+import { initializeAnalytics } from '@/lib/firebase';
 import { logEvent } from 'firebase/analytics';
 
 export default function Home() {
-  // When you want to log an event
-  if (analytics) {
-    logEvent(analytics, 'page_view', {
-      page_title: document.title,
-      page_location: window.location.href,
-    });
-  }
-
   return (
     <main>
       <h1>Welcome to my website</h1>
