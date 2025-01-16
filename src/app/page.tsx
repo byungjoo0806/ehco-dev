1bd2ea96cb4c7272b3802b731c9e83af12eb527d
Byungjoo Park <byungjoopark@Byungjoos-MacBook-Pro-2.local>
2025-01-16 16:01:36 +0900

    still fixing analytics + firebase connection 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
-import { redirect } from 'next/navigation';
+import { analytics } from '@/lib/firebase';
+import { logEvent } from 'firebase/analytics';
 
 export default function Home() {
+  // When you want to log an event
+  if (analytics) {
+    logEvent(analytics, 'page_view', {
+      page_title: document.title,
+      page_location: window.location.href,
+    });
+  }
+
   return (
     <main>
       <h1>Welcome to my website</h1>
